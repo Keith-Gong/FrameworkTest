9eef09399e9946c20a9d4e20c09ebe1952e0054b
Keith <gsh_1234567@126.com>
2015-11-08 13:45:35 +0800

    Initial commit

---
+++ app/src/main/java/keith/com/frameworktest/MainActivity.java
@@ -0,0 +1,16 @@
+package keith.com.frameworktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import will.android.library.net.task.ITask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
